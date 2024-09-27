1892a28b96b9223feb969142ee04cae6b5f0a272
Yash <yashlaxman@gmail.com>
2024-09-27 15:45:50 +0530

    add p2

--- SeqBootCamp/Day3/Yash/p1.c
+++ SeqBootCamp/Day3/Yash/p1.c
@@ -3,45 +3,8 @@
 
 void main()
 {
-    char num[100];
-    scanf("%s", num);
-    for (int i = 0; i < strlen(num); i++)
-    {
-        switch (num[i])
-        {
-        case '0':
-            printf("Zero ");
-            break;
-        case '1':
-            printf("One ");
-            break;
-        case '2':
-            printf("Two ");
-            break;
-        case '3':
-            printf("Three ");
-            break;
-        case '4':
-            printf("Four ");
-            break;
-        case '5':
-            printf("Five ");
-            break;
-        case '6':
-            printf("Six ");
-            break;
-        case '7':
-            printf("Seven ");
-            break;
-        case '8':
-            printf("Eight ");
-            break;
-        case '9':
-            printf("Nine ");
-            break;
-        default:
-            printf("%c ", num[i]);
-            break;
-        }
-    }
+    char str[100];
+    scanf("%s", str);
+    strrev(str);
+    printf("%s", str);
 }
