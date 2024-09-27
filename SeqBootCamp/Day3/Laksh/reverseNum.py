b25204eedaf7eed1bbebe57a09054c274eb82366
LakshAditya <lakshaditys18@gmail.com>
2024-09-27 15:50:42 +0530

    mixed numToWords and reverseNum -Lkash

--- SeqBootCamp/Day3/Laksh/reverseNum.py
+++ SeqBootCamp/Day3/Laksh/reverseNum.py
@@ -1,4 +1,18 @@
-number = input("Enter Number: ")
-number = reversed(number)
-for char in number:
-    print(char, end='')
+def numToWords(number)->None:
+    match number:
+        case 1: print("one", end=" ")
+        case 2: print("two", end=" ")
+        case 3: print("three", end=" ")
+        case 4: print("four", end=" ")
+        case 5: print("five", end=" ")
+        case 6: print("six", end=" ")
+        case 7: print("seven", end=" ")
+        case 8: print("eight", end=" ")
+        case 9: print("nine", end=" ")
+        case 0: print("zero", end=" ")
+
+
+num = input("Enter Number: ")
+num = reversed(num)
+for digit in num:
+    numToWords(int(digit))
