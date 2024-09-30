1ac8b13c9d95a81a0e1c636896d305935fe7f3cf
LakshAditya <lakshaditys18@gmail.com>
2024-09-30 16:21:50 +0530

    idk guys

--- SeqBootCamp/Day4/Laksh/FirstCode.cpp
+++ SeqBootCamp/Day4/Laksh/FirstCode.cpp
@@ -10,13 +10,15 @@ int main(){
     const float itemPrice[]={45.00, 39.00, 1.99, 9.99, 89.99, 22.50, 10.00, 99.00, 75.00, 125.00};
     while(1){
         cout << "Item: " << endl;
-        getline(cin, itemName);
-        if(itemName=="\n") break;
+        cin>>itemName;
         transform(itemName.begin(),itemName.end(),itemName.begin(), ::toupper);
+        if(itemName=="END") break;
         for(i=0; i<11; i++){
-            if(itemList[i]==itemName) break;
+            if(itemList[i]==itemName){
+            charge += itemPrice[i];
+            break;
+            }
         }
-        charge += itemPrice[i];
         cout << "Total Charge is: " << endl;
         cout << charge << endl;
         system("PAUSE");
