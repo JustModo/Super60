0290859d248c9ba18965325beb9bafacd018d32e
Yash <yashlaxman@gmail.com>
2024-10-05 23:44:42 +0530

    complete 51

--- SQLite/Assignment/Solutions/Modo.session.sql
+++ SQLite/Assignment/Solutions/Modo.session.sql
@@ -1,10 +1,10 @@
--- Get all the predictions 
+--1 Get all the predictions 
 SELECT
     *
 FROM
     cardiodiagnosis c;
 
--- Get all the predictions for the day
+--2 Get all the predictions for the day
 SELECT
     *
 FROM
@@ -13,9 +13,9 @@ FROM
 WHERE
     d.diagnoseddate = '2019-01-25';
 
--- Get all the predictions for the day and sort it based on highest percentage of probability at the top
+--3 Get all the predictions for the day and sort it based on highest percentage of probability at the top
 -- ??
--- Get all the unique cities 
+--4 Get all the unique cities 
 SELECT
     UNIQUE(a.city)
 FROM
@@ -23,7 +23,7 @@ FROM
 ORDER BY
     a.city ASC;
 
--- Get all the members who are from a city  'Burgos'
+--5 Get all the members who are from a city  'Burgos'
 SELECT
     *
 FROM
@@ -32,7 +32,7 @@ FROM
 WHERE
     a.city = 'Burgos';
 
--- Get all the members who are from 'flora' city
+--6 Get all the members who are from 'flora' city
 SELECT
     *
 FROM
@@ -41,7 +41,7 @@ FROM
 WHERE
     a.city = 'Flora';
 
--- Get the total number of bloodtests done for Aisha
+--7 Get the total number of bloodtests done for Aisha
 SELECT
     m.firstname AS 'Name',
     COUNT(*) AS 'BloodTestsDone'
@@ -52,7 +52,7 @@ FROM
 WHERE
     m.firstname = 'Aisha';
 
--- Get the xray details of Ajay whose cardio was done on 26th of Jan 2019
+--8 Get the xray details of Ajay whose cardio was done on 26th of Jan 2019
 SELECT
     m.firstname,
     x.ca,
@@ -66,7 +66,7 @@ WHERE
     m.firstname = 'Ajay'
     AND c.date = '2019-01-26';
 
--- Get all the members who are from city burgos and flora
+--9 Get all the members who are from city burgos and flora
 SELECT
     m.firstname,
     a.city
@@ -77,7 +77,7 @@ WHERE
     a.city = 'burgos'
     OR a.city = 'flora';
 
--- Get the total number of bloodtests done for aberson
+--10 Get the total number of bloodtests done for aberson
 SELECT
     COUNT(*)
 FROM
@@ -87,7 +87,7 @@ FROM
 WHERE
     m.lastname = 'aberson';
 
--- Get all address details for member ID M303
+--11 Get all address details for member ID M303
 SELECT
     a.address_id,
     a.city,
@@ -100,7 +100,7 @@ FROM
 WHERE
     m.member_id = 'M303';
 
--- Get all xray details for cardio id cid 122
+--12 Get all xray details for cardio id cid 122
 SELECT
     *
 FROM
@@ -109,7 +109,7 @@ FROM
 WHERE
     cardio_id = 'cid122';
 
--- Get all symptom details whose cardio ID = CID250 and CID300
+--13 Get all symptom details whose cardio ID = CID250 and CID300
 SELECT
     *
 FROM
@@ -119,7 +119,7 @@ WHERE
     c.cardio_id = 'cid250'
     OR c.cardio_id = 'cid300';
 
--- Get all symptom details for month july and year 2019
+--14 Get all symptom details for month july and year 2019
 SELECT
     *
 FROM
@@ -128,7 +128,7 @@ WHERE
     MONTH(date) = '7'
     AND YEAR(date) = '2019';
 
--- Get xray details for member with lastname "dailley"
+--15 Get xray details for member with lastname "dailley"
 SELECT
     *
 FROM
@@ -138,7 +138,7 @@ FROM
 WHERE
     m.lastname = 'dailley';
 
--- Get wearabledevicedata details from cardio ID from CID100 to CID200
+--16 Get wearabledevicedata details from cardio ID from CID100 to CID200
 SELECT
     *
 FROM
@@ -148,7 +148,7 @@ WHERE
     c.cardio_id = 'cid100'
     OR c.cardio_id = 'cid200';
 
--- Display all cardiodiagnosis details who first name start from letter "A"
+--17 Display all cardiodiagnosis details who first name start from letter "A"
 SELECT
     m.firstname,
     c.cardio_id,
@@ -160,7 +160,7 @@ FROM
 WHERE
     m.firstname LIKE 'A%';
 
--- Display all cardiodiagnosis details who first name start from letter "A" and ends at letter "A"
+--18 Display all cardiodiagnosis details who first name start from letter "A" and ends at letter "A"
 SELECT
     m.firstname,
     c.cardio_id,
@@ -173,13 +173,13 @@ WHERE
     m.firstname LIKE 'A%'
     AND m.firstname LIKE '%A';
 
--- To get all the members from the MemberInfo
+--19 To get all the members from the MemberInfo
 SELECT
     username
 FROM
     memberinfo;
 
--- To get all the addresess of Members
+--20 To get all the addresess of Members
 SELECT
     m.username,
     a.city,
@@ -190,19 +190,19 @@ FROM
     memberinfo m
     INNER JOIN addressinfo a ON m.member_id = a.memberinfo_member_id;
 
--- To get a list of wearable device information
+--21 To get a list of wearable device information
 SELECT
     *
 FROM
     wearabledevicedata;
 
--- To get a list of all the blood tests done
+--22 To get a list of all the blood tests done
 SELECT
     *
 FROM
     bloodtest;
 
--- Get a list of members who are aged greater than 50
+--23 Get a list of members who are aged greater than 50
 SELECT
     *
 FROM
@@ -210,7 +210,7 @@ FROM
 WHERE
     age > 50;
 
--- To get list of addresses for the city flora
+--24 To get list of addresses for the city flora
 SELECT
     *
 FROM
@@ -218,25 +218,25 @@ FROM
 WHERE
     city = 'flora';
 
--- Get list of all unique states
+--25 Get list of all unique states
 SELECT
     UNIQUE(a.state) AS 'States'
 FROM
     addressinfo a;
 
--- Get the total number of members in the database
+--26 Get the total number of members in the database
 SELECT
     COUNT(*) AS 'TotalMembers'
 FROM
     memberinfo;
 
--- To get the total number of blood tests done
+--27 To get the total number of blood tests done
 SELECT
     COUNT(*) AS 'TotalBloodTests'
 FROM
     bloodtest;
 
--- To get the average cholestrol level for members
+--28 To get the average cholestrol level for members
 SELECT
     m.username,
     b.serumcholesterol
@@ -245,7 +245,7 @@ FROM
     INNER JOIN cardiodiagnosis c ON c.memberinfo_member_id = m.member_id
     INNER JOIN bloodtest b ON b.cardiodiagnosis_cardio_id = c.cardio_id;
 
--- To get the max peak value in symptoms
+--29 To get the max peak value in symptoms
 SELECT
     MAX(s.oldpeak) as 'MaxPeakValue'
 FROM
@@ -253,7 +253,7 @@ FROM
     INNER JOIN cardiodiagnosis c ON c.memberinfo_member_id = m.member_id
     INNER JOIN symptom s ON c.cardio_id = s.cardiodiagnosis_cardio_id;
 
--- Get the list of tests done between 2015 Jan 1st and 2019 Jan 31st
+--30 Get the list of tests done between 2015 Jan 1st and 2019 Jan 31st
 SELECT
     *
 FROM
@@ -262,7 +262,7 @@ WHERE
     date BETWEEN '2019-01-01'
     AND '2019-01-31';
 
--- Get the number of males and females aged between 50 and 60
+--31 Get the number of males and females aged between 50 and 60
 SELECT
     CASE
         WHEN gender = 0 THEN 'Female'
@@ -277,7 +277,7 @@ WHERE
 GROUP BY
     gender;
 
--- Get the list of tests where blood pressure is between 100 and 200
+--32 Get the list of tests where blood pressure is between 100 and 200
 SELECT
     *
 FROM
@@ -286,19 +286,19 @@ WHERE
     bloodpressure BETWEEN 100
     AND 200;
 
--- Get the list of symptoms diagnosed for patients
+--33 Get the list of symptoms diagnosed for patients
 SELECT
     *
 FROM
     symptom;
 
--- Get the average age of patients in the database
+--34 Get the average age of patients in the database
 SELECT
     AVG(age) AS 'AverageAge'
 FROM
     memberinfo;
 
--- Get the total cities for each state available
+--35 Get the total cities for each state available
 SELECT
     state AS 'State',
     COUNT(city) AS 'NumberOfCities'
@@ -307,7 +307,7 @@ FROM
 GROUP BY
     state;
 
--- Get number of Patients between age group 
+--36 Get number of Patients between age group 
 -- 10-20
 -- 20-30
 -- 30-40
@@ -336,21 +336,21 @@ FROM
 GROUP BY
     AgeRange;
 
--- Get the list of meembers and their address
+--37 Get the list of meembers and their address
 SELECT
     *
 FROM
     memberinfo m
     INNER JOIN addressinfo a ON m.member_id = a.memberinfo_member_id;
 
--- Get the list of meembers and their cardiohistory
+--38 Get the list of meembers and their cardiohistory
 SELECT
     *
 FROM
     memberinfo m
     INNER JOIN cardiodiagnosis c ON m.member_id = c.memberinfo_member_id;
 
--- Get the list of meembers and their diseases
+--39 Get the list of meembers and their diseases
 SELECT
     *
 FROM
@@ -358,7 +358,7 @@ FROM
     INNER JOIN cardiodiagnosis c ON m.member_id = c.memberinfo_member_id
     INNER JOIN diseasedetail d ON d.cardiodiagnosis_cardio_id = c.cardio_id;
 
--- To find the list of female who have been digonised with heart attack
+--40 To find the list of female who have been digonised with heart attack
 SELECT
     *
 FROM
@@ -367,7 +367,7 @@ FROM
 WHERE
     m.gender = 0;
 
--- get the list of female members and their cardio information for people aged above 50
+--41 get the list of female members and their cardio information for people aged above 50
 SELECT
     *
 FROM
@@ -377,7 +377,7 @@ WHERE
     m.gender = 0
     AND age > 50;
 
--- To get the list of males who have their blood pressure > 140 and not had a heart attack
+--42 To get the list of males who have their blood pressure > 140 and not had a heart attack
 SELECT
     *
 FROM
@@ -389,7 +389,7 @@ WHERE
     AND c.cardioarrestdetected = 0
     and m.gender = 1;
 
--- To get the list of members who had heart attack from state "mountain province"
+--43 To get the list of members who had heart attack from state "mountain province"
 SELECT
     *
 FROM
@@ -399,7 +399,7 @@ FROM
 WHERE
     a.state = 'mountain province';
 
--- To get the list of members who are males and their disease with their symptoms for people aged less than 40
+--44 To get the list of members who are males and their disease with their symptoms for people aged less than 40
 SELECT
     *
 FROM
@@ -411,7 +411,7 @@ WHERE
     m.age < 40
     AND m.gender = 1;
 
--- To get count of members from "mountain province" aged between 50 and 60
+--45 To get count of members from "mountain province" aged between 50 and 60
 SELECT
     COUNT(*) AS 'Count'
 FROM
@@ -422,7 +422,7 @@ WHERE
     AND m.age BETWEEN 50
     AND 60;
 
--- To get the count of male and female members who have blood pressure > 140 and detected heart attack
+--46 To get the count of male and female members who have blood pressure > 140 and detected heart attack
 SELECT
     CASE
         WHEN gender = '0' THEN 'Female'
@@ -439,7 +439,7 @@ WHERE
 GROUP BY
     gender;
 
--- Average blood pressure of people aged between 40-50 and 50-60
+--47 Average blood pressure of people aged between 40-50 and 50-60
 SELECT
     CASE
         WHEN m.age BETWEEN 40
@@ -458,7 +458,7 @@ WHERE
 GROUP BY
     AgeRange;
 
--- To get the list of diseases for people having high blood pressure in the range of 120 - 180 sorted across gender
+--48 To get the list of diseases for people having high blood pressure in the range of 120 - 180 sorted across gender
 SELECT
     d.diagnoseddate,
     d.isrecovered,
@@ -475,7 +475,7 @@ WHERE
 ORDER BY
     m.gender;
 
--- To get the count of people who have got their xrays every month from the state of Special Province
+--49 To get the count of people who have got their xrays every month from the state of Special Province
 SELECT
     MONTH(x.date) as month,
     COUNT(*)
@@ -489,7 +489,7 @@ WHERE
 GROUP BY
     month;
 
--- To get the average age of people diagnosed with heart attack for each state across male and female
+--50 To get the average age of people diagnosed with heart attack for each state across male and female
 SELECT
     a.state,
     AVG(
@@ -509,5 +509,21 @@ FROM
 GROUP BY
     a.state;
 
--- To get the count of people for each state having been diagnosed with heart attack who have slope value as 2 and having had atleast 1 xray and 1 symptom
-
+--51 To get the count of people for each state having been diagnosed with heart attack who have slope value as 2 and having had atleast 1 xray and 1 symptom
+SELECT
+    a.state,
+    COUNT(*) AS 'Count'
+FROM
+    memberinfo m
+    INNER JOIN addressinfo a ON m.member_id = a.memberinfo_member_id
+    INNER JOIN cardiodiagnosis c ON m.member_id = c.memberinfo_member_id
+    INNER JOIN symptom s ON s.cardiodiagnosis_cardio_id = c.cardio_id
+    INNER JOIN xray x ON x.cardiodiagnosis_cardio_id = c.cardio_id
+    INNER JOIN wearabledevicedata w ON w.cardiodiagnosis_cardio_id = c.cardio_id
+WHERE
+    w.slope = 2
+GROUP BY
+    a.state
+HAVING
+    COUNT(x.xray_id) >= 1
+    AND COUNT(s.symptom_id) >= 1;
